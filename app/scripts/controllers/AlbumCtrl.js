a1dc67c8e2ee3cc123f4dcbb8e1615d09e53957e
Conrad Pacesa <conradpacesa@gmail.com>
2017-04-18 19:15:38 -0400

    Added songplayer service

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,10 +1,11 @@
 (function() {
-    function AlbumCtrl(Fixtures, Debug) {
+    function AlbumCtrl(Fixtures, SongPlayer) {
         //this.debug = Debug.getDebug();
         this.albumData = Fixtures.getAlbum();
+        this.songPlayer = SongPlayer;
     }
 
     angular
         .module('blocJams')
-        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+        .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
 })();
